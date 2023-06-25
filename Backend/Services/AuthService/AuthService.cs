3f411ffdfb9354d99746c375371e31cab2669257
Filipe Barros <tiagosoarez6@gmail.com>
2023-06-25 09:36:20 +0100

    menu utilizador do sidebar so aparece se for admin FEITO

--- Backend/Services/AuthService/AuthService.cs
+++ Backend/Services/AuthService/AuthService.cs
@@ -102,7 +102,7 @@ public class AuthService : IAuthService
         {
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
             new Claim(ClaimTypes.Name, user.Email),
-            //new Claim(ClaimTypes.Role, user.Role)
+            new Claim(ClaimTypes.Role, user.Role)
         };
 
         var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
